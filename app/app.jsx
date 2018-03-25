43e6a3c75ecee6b07e8f1b5c4a9575e5f568930b
Robert Scully <rtscully@us.ibm.com>
2018-03-25 12:32:48 -0400

    login and logout

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
-import TodoApp from 'TodoApp';
-import Login from 'Login';
+import firebase from 'app/firebase/'
+import router from 'app/router'
+
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    hashHistory.push('/todos')
+  } else {
+    hashHistory.push('/')
+  }
+})
+
 store.dispatch(actions.startAddTodos());
 
 // Load foundation
@@ -17,14 +26,10 @@ $(document).foundation();
 // App css
 require('style!css!sass!applicationStyles')
 
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <Route path="todos" component={TodoApp}/>
-        <IndexRoute component={Login}/>
-      </Route>
-    </Router>
+  <Provider store={ store }>
+    { router }
   </Provider>,
   document.getElementById('app')
 );
